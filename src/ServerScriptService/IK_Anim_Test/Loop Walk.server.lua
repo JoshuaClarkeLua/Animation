c9cadab58631289591928cb61143d3e847113715
TheBexiSS <joshuaclerke1234@gmail.com>
2023-04-02 08:58:58 -0400

    Cleaned up test code

--- src/ServerScriptService/IK_Anim_Test/Loop Walk.server.lua
+++ src/ServerScriptService/IK_Anim_Test/Loop Walk.server.lua
@@ -16,7 +16,7 @@ local animator = humanoid.Animator
 local animTrack = animator:LoadAnimation(WALK_ANIM)
 
 animTrack.Looped = true
-animTrack:Play(0,1,.75)
+animTrack:Play(0,1,.5)
 
 local leftFoot = DUMMY.LeftFoot
 local rightFoot = DUMMY.RightFoot
@@ -35,43 +35,57 @@ params.FilterDescendantsInstances = {DUMMY}
 ]]
 local FOOT_LOCK_DIST = .4
 local FOOT_MAXLOCK_DIST = .1
-local function applyFootIK(rootCF: CFrame, animCF: CFrame, footSize: Vector3, IK: IKControl)
-	local footCF = rootCF*animCF
 
-	-- Cast ray
-	local footDownV = -footCF.UpVector
+local function castLegRay(rootCF: CFrame, footAnimCF: CFrame, footSize: Vector3)
+	local footCF = rootCF*footAnimCF
 	local rayDirection = (footCF.Position-rootCF.Position)
 	local rayDirU = rayDirection.Unit
 	rayDirection = rayDirection+rayDirU*(footSize.Magnitude/2+FOOT_LOCK_DIST)
 	local rayOrigin = rootCF.Position
 	local ray = workspace:Raycast(rayOrigin, rayDirection, params)
+	
+	--[[
+		TODO: IDEA (not super necessary)
+			1. With first ray, check the inclination (normal) of the surface.
+			2. Use the first ray to determine where to shoot the second.
+				Shoot the ray toward the inclination so that we get the smallest distance
+				from the foot to the surface.
+			3. Use the second ray for the distance parameter
+	]]
+
 	if ray then
-		local pos, target: BasePart, normal = ray.Position, ray.Instance, ray.Normal
 		local footDist = ray.Position - footCF.Position + footSize*Vector3.yAxis/2
-		
-		local dist = footDist.Unit:Dot(normal) > 0 and 0 or footDist.Magnitude
-		if dist < FOOT_LOCK_DIST then
-
-			local rot = rootCF.UpVector:Dot(normal)
-			if rot == 1 then IK.Enabled = false; return end
-			rot = math.acos(rot)
-			local axis = rootCF.UpVector:Cross(normal).Unit
-			if axis.Magnitude ~= axis.Magnitude then IK.Enabled = false; return end
-			local incline = axis:Cross(normal)
-			local weight = 1-dist/FOOT_LOCK_DIST
-
-			IK.Enabled = true
-			IK.Target = target
-			IK.Offset = target.CFrame.Rotation:Inverse()
-			IK.EndEffectorOffset = CFrame.fromAxisAngle(axis, -rot)
-			IK.Weight = weight
-
-			-- Debug.drawRay(rayOrigin, rayDirection, true).Color = Color3.new(1,0,0)
-			-- Debug.drawRay(footCF.Position, incline, true).Color = Color3.new(0,0,1)
-		else
-			IK.Enabled = false
-		end
+		local distance = footDist.Unit:Dot(ray.Normal) > 0 and 0 or footDist.Magnitude
+		return true, ray.Instance, ray.Normal, distance, ray.Position
 	end
+	return false
+end
+
+local function applyFootIK(rootCF: CFrame, footAnimCF: CFrame, IK: IKControl, footSize: Vector3)
+	local enabled, target, normal, distance = castLegRay(rootCF, footAnimCF, footSize)
+	local angle, rotAxis, weight
+
+	IK.Enabled = false
+	if not enabled then return end
+	if distance > FOOT_LOCK_DIST then return end
+	if rootCF.Position:Dot(normal) == 1 then return end
+
+	angle = math.acos(rootCF.UpVector:Dot(normal))
+	if angle == 1 then return end
+	rotAxis = rootCF.UpVector:Cross(normal).Unit
+	if rotAxis.Magnitude ~= rotAxis.Magnitude then return end
+	weight = 1-distance/FOOT_LOCK_DIST
+
+	IK.Enabled = true
+	IK.Target = target
+	IK.Offset = target.CFrame.Rotation:Inverse()
+	IK.EndEffectorOffset = CFrame.fromAxisAngle(rotAxis, -angle)
+	IK.Weight = weight
+	IK.Enabled = true
+end
+
+local function applyLegIK(rootCF: CFrame, animCF: CFrame, IK: IKControl)
+	
 end
 
 --[[
@@ -83,9 +97,8 @@ end
 ]]
 local function preIK(dt: number)
 	local animCFs = AssemblyUtil.getAssemblyAnimCFrames(AssemblyUtil.getAssemblyMotors(DUMMY))
-	applyFootIK(rootPart.CFrame, animCFs[leftFoot], leftFoot.Size, IK['LeftFoot'])
-	applyFootIK(rootPart.CFrame, animCFs[rightFoot], rightFoot.Size, IK['RightFoot'])
-	--applyFootIK2(rootPart.CFrame, animCFs[leftFoot], leftFoot, leftFoot.LeftAnkle)
+	applyFootIK(rootPart.CFrame, animCFs[leftFoot], IK['LeftFoot'], leftFoot.Size)
+	applyFootIK(rootPart.CFrame, animCFs[rightFoot], IK['RightFoot'], leftFoot.Size)
 end
 
 local function postIK(dt: number)
